283add3fc52b8c9dd22b1ff03a622025c8171b2d
ddo54 <ddo54@uclive.ac.nz>
2026-08-05 23:57:01 +1200

    Reduced repetition in file reads and write dataset out to folder.

--- airbnb christchurch dataset.R
+++ airbnb christchurch dataset.R
@@ -2,83 +2,25 @@ library(readr)
 library(dplyr)
 library(lubridate)
 
-listings_jun <- read_csv("../data_d3/listings_2026-06.csv")
+# Setup date metadata
+dates <- c("2025-10", "2025-11", "2025-12", "2026-01", "2026-02", "2026-03", "2026-04", "2026-05", "2026-06")
+labels <- c("October 2025", "November 2025", "December 2025", "January 2026", "February 2026", 
+            "March 2026", "April 2026", "May 2026", "June 2026")
 
-jun_chch <- listings_jun %>%
-  filter(neighbourhood_group == "Christchurch City") %>%
-  mutate(month_year = "June 2026")   #add in month + time
+# Initialise list to store data frames
+data_list <- vector("list", length(dates))
 
+# Loop over files
+for (i in seq_along(dates)) {
+  data_list[[i]] <- read_csv(paste0("../data_d3/listings_", dates[i], ".csv"), show_col_types = FALSE) %>%
+    filter(neighbourhood_group == "Christchurch City") %>%
+    mutate(month_year = labels[i])
+}
 
-listings_may <- read_csv("../data_d3/listings_2026-05.csv")
+# Combine into single data frame
+airbnb <- bind_rows(data_list)
 
-may_chch <- listings_may %>%
-  filter(neighbourhood_group == "Christchurch City") %>%
-  mutate(month_year = "May 2026")
-
-
-listings_apr <- read_csv("../data_d3/listings_2026-04.csv")
-
-apr_chch <- listings_apr %>%
-  filter(neighbourhood_group == "Christchurch City") %>%
-  mutate(month_year = "April 2026")
-
-
-listings_mar <- read_csv("../data_d3/listings_2026-03.csv")
-
-mar_chch <- listings_mar %>%
-  filter(neighbourhood_group == "Christchurch City") %>%
-  mutate(month_year = "March 2026")
-
-
-listings_feb <- read_csv("../data_d3/listings_2026-02.csv")
-
-feb_chch <- listings_feb %>%
-  filter(neighbourhood_group == "Christchurch City") %>%
-  mutate(month_year = "February 2026")
-
-
-listings_jan <- read_csv("../data_d3/listings_2026-01.csv")
-
-jan_chch <- listings_jan %>%
-  filter(neighbourhood_group == "Christchurch City") %>%
-  mutate(month_year = "January 2026")
-
-
-listings_dec <- read_csv("../data_d3/listings_2025-12.csv")
-
-dec_chch <- listings_dec %>%
-  filter(neighbourhood_group == "Christchurch City") %>%
-  mutate(month_year = "December 2025")
-
-
-listings_nov <- read_csv("../data_d3/listings_2025-11.csv")
-
-nov_chch <- listings_nov %>%
-  filter(neighbourhood_group == "Christchurch City") %>%
-  mutate(month_year = "November 2025")
-
-
-listings_oct <- read_csv("../data_d3/listings_2025-10.csv")
-
-oct_chch <- listings_oct %>%
-  filter(neighbourhood_group == "Christchurch City") %>%
-  mutate(month_year = "October 2025")
-
-
-
-airbnb <- bind_rows(
-  oct_chch,
-  nov_chch,
-  dec_chch,
-  jan_chch,
-  feb_chch,
-  mar_chch,
-  apr_chch,
-  may_chch,
-  jun_chch
-)   #combine the data sets
-
-summary(airbnb)    #summary including max, min, mean, catagories
+summary(airbnb)    #summary including max, min, mean, categories
 
 colSums(is.na(airbnb))    #better for just seeing where the NAs are
 
@@ -88,6 +30,7 @@ airbnb %>%
 
 table(airbnb$room_type)
 table(airbnb$neighbourhood)
-table(airbnb$month_year)     #tables to have a better look at catagorical variables and see where data came from
+table(airbnb$month_year)     #tables to have a better look at categorical variables and see where data came from
 
+write_csv(airbnb, "../data_d3/listings_chch.csv")
 
