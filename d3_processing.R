fda757ff3613ca6c487785c107b7fb12a6dc8dbf
ddo54 <ddo54@uclive.ac.nz>
2026-08-12 19:50:40 +1200

    Fixed formatting in last review script and deleted test data_d3 processing file.

--- d3_processing.R
+++
@@ -1,2 +0,0 @@
-df <- read.csv("../data_d3/listings_2026-06.csv")
-
--- last_review_days.R
+++ last_review_days.R
@@ -19,20 +19,29 @@ review_data <- review_data %>%
 
 
 scrape_lookup <- tibble(
-  month_year = c("October 2025", "November 2025", "December 2025", "January 2026", "February 2026", 
-                          "March 2026", "April 2026", "May 2026", "June 2026"), 
+  month_year = c(
+    "October 2025", 
+    "November 2025", 
+    "December 2025", 
+    "January 2026", 
+    "February 2026", 
+    "March 2026", 
+    "April 2026", 
+    "May 2026", 
+    "June 2026"
+    ), 
   scrape_date = as.Date(c(
     "2025-10-05", 
-  "2025-11-25", 
-  "2025-12-11", 
-  "2026-01-16", 
-  "2026-02-13", 
-  "2026-03-17", 
-  "2026-04-16", 
-  "2026-05-23", 
-  "2026-06-19"
-)))
-
+    "2025-11-25", 
+    "2025-12-11", 
+    "2026-01-16", 
+    "2026-02-13", 
+    "2026-03-17", 
+    "2026-04-16", 
+    "2026-05-23", 
+    "2026-06-19"
+    ))
+  )
 
 review_data <- review_data %>% 
   left_join(scrape_lookup, by = "month_year") %>% 
