5d7e58f356ac1ff5aceb4544d07c83e8fdb36aac
Lucious pinto <luciousppinto@gmail.com>
2026-08-12 14:21:41 +1200

    commiting d3 and hist

--- d3_processing.R
+++ d3_processing.R
@@ -1,1 +1,2 @@
 df <- read.csv("../data_d3/listings_2026-06.csv")
+
--- lucious-price-histogram.R
+++ lucious-price-histogram.R
@@ -1,6 +1,9 @@
 library(ggplot2)
+library(readr)
 
-df <- read.csv("../data_d3/listings_2026-06.csv")
+
+# Load concatenated Christchurch dataset
+df <- read_csv("../data_d3/listings_chch.csv")
 
 # Remove missing prices
 df_clean <- df[!is.na(df$price), ]
@@ -12,20 +15,12 @@ IQR_val <- Q3 - Q1
 df_clean <- df_clean[df_clean$price >= Q1 - 1.5 * IQR_val & 
                        df_clean$price <= Q3 + 1.5 * IQR_val, ]
 
-# NZ histogram
-ggplot(df_clean, aes(x = price)) +
-  geom_histogram(fill = "#7B7FC4", color = "white", bins = 40) +
-  labs(title = "Distribution of price for Airbnb NZ",
-       x = "price", y = "count") +
-  theme_minimal()
-
-# Filter for Christchurch
-df_chch <- df_clean[df_clean$neighbourhood_group == "Christchurch City", ]
-
 # Christchurch histogram
-ggplot(df_chch, aes(x = price)) +
+ggplot(df_clean, aes(x = price)) +
   geom_histogram(fill = "#7B7FC4", color = "white", bins = 40) +
   labs(title = "Distribution of price for Airbnb Christchurch",
        x = "price", y = "count") +
   theme_minimal()
+
+
 
