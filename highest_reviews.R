787f0e8dae7cb7e5712bb7f93876ea753a86f589
ddo54 <ddo54@uclive.ac.nz>
2026-08-12 14:48:29 +1200

    Added ability to plot a price histogram

--- highest_reviews.R
+++ highest_reviews.R
@@ -1,6 +1,6 @@
 library(readr)
 library(dplyr)
-# library(ggplot2)
+library(ggplot2)
 
 most_reviews <- read_csv("../data_d3/listings_chch.csv") %>%
   slice_max(order_by = number_of_reviews, prop = 0.1)
@@ -12,19 +12,16 @@ summary_top <- most_reviews %>%
     min_num_reviews = min(number_of_reviews, na.rm = TRUE),
     mean_num_reviews = mean(number_of_reviews, na.rm = TRUE),
     max_num_reviews = max(number_of_reviews, na.rm = TRUE),
-    min_price = min(price, na.rm = TRUE),
-    mean_price = mean(price, na.rm = TRUE),
-    max_price = max(price, na.rm = TRUE)
   )
 
 print(summary_top)
 
 
-# ggplot(filtered_data, aes(x = price)) +
-#   geom_histogram(bins = 30, fill = "steelblue", color = "white", alpha = 0.8) +
-#   labs(
-#     title = "Price Distribution of Top 10% Filtered Rows",
-#     x = "Price",
-#     y = "Count"
-#   ) +
-#   theme_minimal()
+ggplot(most_reviews, aes(x = price)) +
+  geom_histogram(bins = 50, fill = "steelblue", color = "white", alpha = 0.8) +
+  labs(
+    title = "Price Distribution of top 10% listings by review count in Christchurch",
+    x = "Price",
+    y = "Count"
+  ) +
+  theme_minimal()
