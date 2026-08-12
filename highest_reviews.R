c74026a992d6d6d5ec6e3f41ac27de5b725a9e77
ddo54 <ddo54@uclive.ac.nz>
2026-08-12 14:30:41 +1200

    Works on concatonated dataset and added option to plot histogram.

--- highest_reviews.R
+++ highest_reviews.R
@@ -1,9 +1,12 @@
 library(readr)
 library(dplyr)
+# library(ggplot2)
 
-summary_top <- read_csv("../data_d3/listings_chch.csv") %>%
-  slice_max(order_by = number_of_reviews, prop = 0.1) %>%
-  summarise(
+most_reviews <- read_csv("../data_d3/listings_chch.csv") %>%
+  slice_max(order_by = number_of_reviews, prop = 0.1)
+
+
+summary_top <- summarise(
     count = n(),
     min_num_reviews = min(number_of_reviews, na.rm = TRUE),
     mean_num_reviews = mean(number_of_reviews, na.rm = TRUE),
@@ -14,3 +17,13 @@ summary_top <- read_csv("../data_d3/listings_chch.csv") %>%
   )
 
 print(summary_top)
+
+
+# ggplot(filtered_data, aes(x = price)) +
+#   geom_histogram(bins = 30, fill = "steelblue", color = "white", alpha = 0.8) +
+#   labs(
+#     title = "Price Distribution of Top 10% Filtered Rows",
+#     x = "Price",
+#     y = "Count"
+#   ) +
+#   theme_minimal()
