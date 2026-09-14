186367bdb31d01ad32af8b5d703c63c54769b0ef
Alyssa Thompson <alyssa.thompson246@gmail.com>
2026-09-15 11:46:57 +1200

    removing na values from minimum_nights

--- airbnb_combined_cleaning_d4.R
+++ airbnb_combined_cleaning_d4.R
@@ -12,5 +12,9 @@ airbnb_clean <- airbnb %>%
   select(id, name, room_type, latitude, longitude, room_type, price,
          minimum_nights, availability_365, month_year)
 
+# Removing the na values from minimum_nights
+airbnb_clean <- airbnb_clean %>%
+  filter(!is.na(minimum_nights))
+
 # Save the cleaned file
 write_csv(airbnb_clean, "../data_d3/listings_chch.csv")
