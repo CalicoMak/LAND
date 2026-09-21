3d1a32e40d82f360771c6f9be323393a83df4853
Alyssa Thompson <alyssa.thompson246@gmail.com>
2026-09-22 10:46:01 +1200

    Editing Lucious' code to allow for joining by time feature ontop of his join by location.

--- sqlite_join_rentals.R
+++ sqlite_join_rentals.R
@@ -1,6 +1,8 @@
 library(DBI)
 library(RSQLite)
 library(readr)
+library(dplyr)
+library(lubridate)
 
 # 1. Define parent directory path
 data.dir <- "../data_d3"
@@ -13,6 +15,14 @@ bonds <- read_csv(file.path(data.dir, "bonds_chch.csv"))
 listings$sa2_code <- as.character(listings$sa2_code) 
 bonds$`Location Id` <- as.character(bonds$`Location Id`)
 
+listings <- listings %>%
+  mutate(timeframe = as.Date(timeframe), 
+         quarter = case_when(
+           month(timeframe) %in% c(10,11,12) ~ "2025-10-01", 
+           month(timeframe) %in% c(1, 2, 3) ~ "2026-01-01", 
+           month(timeframe) %in% c(4,5,6) ~ "2026-04-01"
+         ))
+
 # 4. Create SQLite database file inside the data folder
 con <- dbConnect(RSQLite::SQLite(), file.path(data.dir, "rentals.db"))
 
@@ -22,14 +32,24 @@ dbWriteTable(con, "bonds", bonds, overwrite = TRUE)
 
 # 6. SQL Join Query using exact column names
 sql_join <- "
-SELECT 
+WITH bonds_agg AS (
+    SELECT
+        `Location Id`,
+        TimeFrame,
+        SUM(`Median Rent` * `Total Bonds`) * 1.0 / SUM(`Total Bonds`) AS weighted_median_rent
+    FROM bonds
+    GROUP BY `Location Id`, TimeFrame
+)
+SELECT
     a.id AS airbnb_id,
     a.sa2_code,
+    a.quarter,
     a.price AS airbnb_price_per_night,
-    b.`Median Rent` AS bond_median_weekly_rent
+    b.weighted_median_rent AS bond_median_weekly_rent
 FROM airbnb a
-INNER JOIN bonds b 
-    ON a.sa2_code = b.`Location Id`;
+INNER JOIN bonds_agg b
+    ON a.sa2_code = b.`Location Id`
+    AND a.quarter = b.TimeFrame;
 "
 
 joined_data <- dbGetQuery(con, sql_join)
@@ -39,3 +59,4 @@ dbDisconnect(con)
 
 # Preview results
 head(joined_data)
+
